d8b24d579bbecb232135f59f5d03c5bdc34dc605
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-07-12 13:07:39 +0100

    event binding et property binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,22 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+
+  logo:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMcOuz17dyeXVHeUdUgaSRcvgDb7hO06tGjw&s";
   formation:string="FullStack Spring Angular & Devops";
 
   nom:string  ="Mohamed Amine mezghich";
   email:string ="amine.mezghich@gmail.com";
 
   noms:string[]=["aMINE","taHa","abdeSAtar","raniA","iNes"]
+
+  displaySuccess()
+  {
+    alert("Tu as cliquer sur btn success")
+  }
+
+   displayDanger()
+  {
+    alert("Tu as cliquer sur btn danger")
+  }
 }
